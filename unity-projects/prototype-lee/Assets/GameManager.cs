3008cb89d262400807fc9f70a57cfea946bbef0d
fmvlee <lee@javastudent.co.uk>
2022-12-15 22:27:46 +0000

    Lee Protoype Added
    Added my prototype project

---
+++ unity-projects/prototype-lee/Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject playerOne;
+    public GameObject playerOneBG;
+    public GameObject playerTwo;
+    public GameObject playerTwoBG;
+    public Camera followCam;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log("Player One Selected");
+            playerOneBG.SetActive(true);
+            playerTwoBG.SetActive(false);
+            followCam.GetComponent<FollowObject>().targetObject = playerOne.transform;
+            playerOne.GetComponent<mover>().enabled = true;
+            playerTwo.GetComponent<mover>().enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log("Player Two Selected");
+            playerOneBG.SetActive(false);
+            playerTwoBG.SetActive(true);
+            followCam.GetComponent<FollowObject>().targetObject = playerTwo.transform;
+            playerOne.GetComponent<mover>().enabled = false;
+            playerTwo.GetComponent<mover>().enabled = true;
+        }
+    }
+}
---
+++ unity-projects/prototype-lee/Assets/PressureSwitch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressureSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private float triggerMass = 3;
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player") && collision.collider.GetComponent<Rigidbody>().mass >= triggerMass)
+        {
+            Debug.Log("collision");
+        }
+    }
+}
