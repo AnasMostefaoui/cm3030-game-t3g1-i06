77d38d2986931a03161e70327b9b585b4c94abdf
fmvlee <lee@javastudent.co.uk>
2023-01-28 14:15:25 +0000

    Added Wall Switch improved player switching

--- final-project/Kindred-spirit/Assets/Scripts/InputController.cs
+++ final-project/Kindred-spirit/Assets/Scripts/InputController.cs
@@ -6,24 +6,15 @@ using UnityEngine.Events;
 public class InputController : MonoBehaviour
 {
     // Events to be called
-    public UnityEvent onGhostSelectEvent;
-    public UnityEvent onPlayerSelectEvent;
     public UnityEvent onPause;
     public UnityEvent onUnPause;
 
     // Check for a key press and invoke relevent event
     private void LateUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1) && GameManager.Instance.isGhostSelected == true)
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            onPlayerSelectEvent.Invoke();
-            GameManager.Instance.isGhostSelected = false;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2) && GameManager.Instance.isGhostSelected == false)
-        {
-            onGhostSelectEvent.Invoke();
-            GameManager.Instance.isGhostSelected  = true;
+            GameManager.Instance.CharacterSwitch();
         }
 
         // Press Escape key for pause menu
---
+++ final-project/Kindred-spirit/Assets/Scripts/WallSwitch.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WallSwitch : MonoBehaviour
+{
+    public UnityEvent wallSwitchOn;
+    public UnityEvent wallSwitchOff;
+    private bool isSwitched = false;
+
+    [SerializeField]
+    private bool isToggleSwitch = false;
+
+    // Hels handling the trigger
+    private bool isWithinRange = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isWithinRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isWithinRange = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isWithinRange)
+        {
+            if (!isSwitched)
+            {
+                wallSwitchOn.Invoke();
+                isSwitched = true;
+            } else {
+                if (isToggleSwitch)
+                {
+                    wallSwitchOff.Invoke();
+                    isSwitched = false;
+                }
+            }
+        }
+    }
+}
