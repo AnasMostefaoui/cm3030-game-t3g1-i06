8ea626ee0b8efa8c606da15f43c396c2d8ac51bc
Mohamed Anes Mostefaoui <m.anas@outlook.com>
2023-02-03 14:47:02 +0300

    First draft of pushing platform

---
+++ final-project/Kindred-spirit/Assets/Scripts/MoveableObjects.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableObjects : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
