33fd791e9fa35081862cfee7397d83e818105adf
christianb3ll <christian.b311@gmail.com>
2023-02-17 20:38:57 +0000

    Various sound fixes
    Add footsteps to player prefab
    Add menu UI sounds
    Fix old door prefab

---
+++ final-project/Kindred-spirit/Assets/Scripts/Sound/UISounds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISounds : MonoBehaviour
+{
+    [SerializeField]
+    GameObject selectSoundObj;
+    AudioSource selectSound;
+
+    [SerializeField]
+    GameObject confirmSoundObj;
+    AudioSource confirmSound;
+
+    private void Start()
+    {
+        selectSound = selectSoundObj.GetComponent<AudioSource>();
+        confirmSound = confirmSoundObj.GetComponent<AudioSource>();
+    }
+
+
+    public void PlaySelect()
+    {
+        selectSound.Play();
+    }
+
+    public void PlayConfirm()
+    {
+        confirmSound.Play();
+    }
+}
