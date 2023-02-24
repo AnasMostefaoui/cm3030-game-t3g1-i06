ba68fa7a7320d65b6e260df2ac8d60929ccdb8b2
christianb3ll <christian.b311@gmail.com>
2023-02-24 17:02:41 +0000

    refactor spirit link code and add prefabs

--- final-project/Kindred-spirit/Assets/Scripts/Managers/SpiritManager.cs
+++ final-project/Kindred-spirit/Assets/Scripts/Managers/SpiritManager.cs
@@ -31,17 +31,17 @@ public class SpiritManager : MonoBehaviour
 
     // The line showing the visual spirit link
     public bool spiritLineActive = false;
-    public GameObject spiritLineObj;
+    public GameObject spiritLinkObj;
     LineRenderer spiritLine;
 
     GameObject humanChar;
     GameObject ghostChar;
 
-    public GameObject humanSpiritGlow;
-    public GameObject ghostSpiritGlow;
+    GameObject humanSpiritGlow;
+    GameObject ghostSpiritGlow;
 
-    public Material glowMaterial;
-    public Material lineMaterial;
+    Material glowMaterial;
+    Material lineMaterial;
 
     private void Start()
     {
@@ -49,11 +49,28 @@ public class SpiritManager : MonoBehaviour
         spiritHealth = maxSpiritHealth;
 
         // initialise the spirit link line
-        spiritLine = spiritLineObj.GetComponent<LineRenderer>();
+        spiritLine = spiritLinkObj.GetComponent<LineRenderer>();
 
         // initialise character object references
         humanChar = GameManager.Instance.GetHumanPlayer();
         ghostChar = GameManager.Instance.GetGhostPlayer();
+
+        // retreive the spirit glow objects
+        humanSpiritGlow = humanChar.transform.Find("SpiritGlow").gameObject;
+        ghostSpiritGlow = ghostChar.transform.Find("SpiritGlow").gameObject;
+
+        //  Get the materials
+        glowMaterial = humanSpiritGlow.GetComponent<MeshRenderer>().material;
+        lineMaterial = spiritLine.material;
+
+        // Make a copy of the materials so we can edit values non-destructively
+        glowMaterial = new Material(glowMaterial);
+        lineMaterial = new Material(lineMaterial);
+
+        // set the new materials
+        humanSpiritGlow.GetComponent<MeshRenderer>().material = glowMaterial;
+        ghostSpiritGlow.GetComponent<MeshRenderer>().material = glowMaterial;
+        spiritLine.material = lineMaterial;
     }
 
     private void Update()
@@ -90,10 +107,10 @@ public class SpiritManager : MonoBehaviour
             Vector3 particlePos = (humanChar.transform.position + ghostChar.transform.position) / 2;
 
             // Set particle emitter to midpoint
-            spiritLineObj.transform.position = particlePos + Vector3.up;
+            spiritLinkObj.transform.position = particlePos + Vector3.up;
 
             // Play the particle explosion
-            spiritLineObj.GetComponent<ParticleSystem>().Play();
+            spiritLinkObj.GetComponent<ParticleSystem>().Play();
 
             
             spiritLine.enabled = false;
@@ -115,6 +132,8 @@ public class SpiritManager : MonoBehaviour
 
         if (glowMaterial.GetFloat("_Transparency") < 1)
         {
+            humanSpiritGlow.SetActive(true);
+            ghostSpiritGlow.SetActive(true);
             StartCoroutine(GlowFadeIn());
         }
     }
